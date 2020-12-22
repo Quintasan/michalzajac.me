3c3cf8d64aeb7b1d5f63e1976d2fe3d769190403
Michał Zając <michal.zajac@gmail.com>
2020-12-22 22:49:27 +0100

    Add prelink to GFonts and use webp

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
+        test: /\.(webp|png|svg|jpg|gif)$/,
         loader: 'file-loader',
         options: {
           name: '[path][name].[ext]',
