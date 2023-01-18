c9de5b23d60fef9562eaf92e5b54dde1defc5c8b
Michał Zając <michal.zajac@gmail.com>
2023-01-18 15:11:03 +0100

    Move to esbuild

--- config.rb
+++ config.rb
@@ -5,22 +5,19 @@ helpers ArticleHelpers
 set :markdown_engine, :common_marker
 activate :directory_indexes
 
-page "/", layout: "home_page_layout"
-page "/blog.html", layout: "blog_layout"
-page "/talks.html", layout: "post_layout"
+page '/', layout: 'home_page_layout'
+page '/blog.html', layout: 'blog_layout'
+page '/talks.html', layout: 'post_layout'
 
 activate :external_pipeline,
-   name: :webpack,
-   command: build? ? 'npm run build' : 'npm run start',
-   source: '.tmp/dist',
-   latency: 1
-
-config[:js_dir] = 'assets/javascripts'
-config[:css_dir] = 'assets/stylesheets'
+         name: 'esbuild',
+         command: build? ? 'npm run build' : 'npm run start',
+         source: './dist',
+         latency: 1
 
 activate :blog do |blog|
-  blog.prefix = "blog"
-  blog.layout = "post_layout"
+  blog.prefix = 'blog'
+  blog.layout = 'post_layout'
 end
 
 configure :build do
@@ -29,7 +26,7 @@ end
 
 activate :deploy do |deploy|
   deploy.deploy_method = :rsync
-  deploy.host = "michalzajac.me"
-  deploy.path = "/srv/www/michalzajac.me"
+  deploy.host = 'michalzajac.me'
+  deploy.path = '/srv/www/michalzajac.me'
   deploy.build_before = true
 end
